72ffbb30c415997117ea67403a7fe04ff2f32ee9
egarsan176 <estefaniagarci@gmail.com>
2021-03-16 20:21:07 +0100

    16-03. ejerc2, rel8_arrays por completar

--- relacion 8 ARRAYS/rel_8_arrays_2/DesplazarPosicion.java
+++ relacion 8 ARRAYS/rel_8_arrays_2/DesplazarPosicion.java
@@ -1,18 +1,55 @@
 package rel_8_arrays_2;
 
+import java.util.Scanner;
+
+import pruebaMATRIZ.Matriz;
+
 public class DesplazarPosicion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		int [] vector = {0,1,2,3,4,5,6,7,8,9};
-		desplazarPosicion(vector);
+		
+		//System.out.println(Matriz.imprimirEnString2(pedirNumeros()));
+		
+		System.out.println(Matriz.imprimirEnString2(desplazarPosicion(pedirNumeros())));
+		
 	}
 
-	public static void desplazarPosicion(int [] vector) {
+	/**
+	 * pide por teclado una lista de 10 numeros enteros
+	 * @return lista
+	 */
+    public static int [] pedirNumeros() { //falta comprobar números
+    	
+    	Scanner teclado = new Scanner(System.in);
+		int[] listaNumeros = new int[10];
+    	
+        for (int i = 0; i < listaNumeros.length; i++) {
+            System.out.print("Introduce un número entero: ");
+            listaNumeros[i] = Integer.parseInt(teclado.nextLine());
+        }
+        teclado.close();
+        return listaNumeros;
+    }
+	
+	
+	public static int [] desplazarPosicion(int [] listaNumeros) { //sin acabar
 		
+		int ultimo;
+        
+        ultimo=listaNumeros[listaNumeros.length-1]; //guardo el último elemento del vector
+        
+        //for(){
+            
+        //}
+        //pongo en primera pos la variable ultimo
+        listaNumeros[0]=ultimo;
+        
+        return listaNumeros;
+        
+        
+    }
 		
 
-	}
 	
 }
