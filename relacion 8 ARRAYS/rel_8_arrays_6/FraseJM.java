2626b80b74790e0edcfe2d8a554156a5216f7869
egarsan176 <estefaniagarci@gmail.com>
2021-04-08 10:09:25 +0200

    08-04. Añadido el ejercicio 6 de la relación 8, Frase.java, versión de clase

---
+++ relacion 8 ARRAYS/rel_8_arrays_6/FraseJM.java
@@ -0,0 +1,44 @@
+package rel_8_arrays_6;
+
+public class FraseJM {
+
+	private final String CARACTERES = "abcdefghijklmnñopqrstuvwxyz";
+	private int[] ocurrencias = new int[CARACTERES.length()];
+	/**
+	 * Consulta en el array de caracteres la ocurrencia de un caracater
+	 * pasado como parametro e incrementa su contador
+	 * @param c caracter a comprobar
+	 */
+	private void contarOcurrencias(char c) {
+		for (int i=0; i<CARACTERES.length();i++) {
+			if(Character.toLowerCase(CARACTERES.charAt(i))==Character.toLowerCase(c)){
+				ocurrencias[i]++;
+			}
+		}
+		
+	}
+	
+	private void contarOcurrenciasFrase(String frase) {
+		for(int i = 0; i<frase.length();i++) {
+			contarOcurrencias(frase.charAt(i));
+		}
+	}
+	
+	
+	public String mostrarOcurrencias(String frase) {
+		StringBuilder mensaje = new StringBuilder();
+		
+		contarOcurrenciasFrase(frase);
+		
+		for (int i=0; i<ocurrencias.length;i++) {
+			if(ocurrencias[i]>0) {
+				
+				mensaje.append("La letra "+CARACTERES.charAt(i)+" ha aparecido "+ocurrencias[i]+(ocurrencias[i]>1 ? " veces.": " vez.")+"\n");
+			}
+		}
+		
+		
+		return mensaje.toString();
+	}
+	
+}
---
+++ relacion 8 ARRAYS/rel_8_arrays_6/MainLetrasJM.java
@@ -0,0 +1,15 @@
+package rel_8_arrays_6;
+
+public class MainLetrasJM {
+
+	public static void main(String[] args) {
+	
+		
+		
+		
+		System.out.println(new FraseJM().mostrarOcurrencias("Hola buenos dias"));
+
+		
+	}
+
+}
